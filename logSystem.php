7cd4c11bfc76eb277651c24f9b7e7f3307fa2dba
Kidus <kidusmekuria11@gmail.com>
2025-04-16 20:20:45 +0300

    Added log system

--- logSystem.php
+++ logSystem.php
@@ -1,11 +1,15 @@
 <?php
 //simple log system
-$logFile = fopen("visits.log", "a") or die("enable to opne file");
+//$logFile = fopen("visits.log", "a") or die("enable to opne file");
 
-$logEntry = "visits from IP: " . $_SERVER['REMOTE_ADDR'] . "on" . date("Y-m-d H:i:s") . "\n";
+//$logEntry = "visits from IP: " . $_SERVER['REMOTE_ADDR'] . "on" . date("Y-m-d H:i:s") . "\n";
 
-fwrite($logFile, $logEntry);
+$handle = fopen("visits.log", "r");
+while(!feof($handle)){
+    echo fgets($handle);
+}
 
-fclose($logFile);
+
+fclose($handle);
 
 ?>
